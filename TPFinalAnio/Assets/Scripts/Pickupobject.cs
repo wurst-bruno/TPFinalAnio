77a9c628604c9f72f0f53b26cb8b7c5b56cd3753
nicolas-chab <47203227@est.ort.edu.ar>
2022-11-09 19:02:47 -0300

    estatuilla and pickupobject implemented

---
+++ TPFinalAnio/Assets/Scripts/Pickupobject.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pickupobject : MonoBehaviour
+{
+    public Transform destination;
+    private void OnMouseDown()
+    {
+        GetComponent<BoxCollider>().enabled = true;
+        GetComponent<Rigidbody>().useGravity = false;
+        this.transform.position = destination.position;
+        this.transform.parent = GameObject.Find("destinationofobj").transform;
+    }
+    void OnMouseUp()
+    {
+        GetComponent<BoxCollider>().enabled = true;
+        this.transform.parent = null;
+        GetComponent<Rigidbody>().useGravity = true;
+    }
+}
